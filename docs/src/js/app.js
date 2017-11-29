94777889561694213fd4a2248fbba728bdfb9015
Huy Nguyen <huy.nguyen@sentifi.com>
2017-11-29 14:22:33 +0700

    Update architecture documentation

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Rext from '~/rext';
 import Viewport from './components/viewport/viewport';
 // import DataPackage from './components/core-concepts/data-package';
-// import Architecture from './components/architecture/architecture';
 // import GridComponent from './components/ui-components/grid';
 
 Rext.application({
@@ -11,6 +10,7 @@ Rext.application({
     require('./components/getting-started/getting-started'),
     require('./components/core-concepts/component'),
     require('./components/core-concepts/screen-navigation'),
+    require('./components/architecture/architecture'),
     require('./components/example/dashboard'),
     require('./components/example/search'),
     require('./components/example/details'),
--- docs/src/js/components/architecture/architecture.jsx
+++ docs/src/js/components/architecture/architecture.jsx
@@ -1,70 +1,61 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Route, Container, String } from '~/rext';
 
 @Route('/architecture')
-export default class Architecture extends Component {
+export default class Architecture extends PureComponent {
   render() {
-    return <Container>
-      <div className="panel-header">
-        <h1 className="panel-title">Application Architecture</h1>
-      </div>
-      <Container className="panel-body">
-        <p>
-          Extension React provides support for MVC+VM application architectures. To understand what is MVC+VM, we should start by further defining what the various abbreviations represent.
-        </p>
-        <p>
-          <ul className="list-style-disc">
-            <li>
-              <strong>(M) Model</strong> - This is the data for your application.
-              A set of classes (called “Models”) defines the fields for their data (e.g. a User model with user-name and password fields).
-              Models know how to persist themselves through the data package and can be linked to other models via associations.
-              Models are normally used in conjunction with Stores to provide data for grids and other components.
-              Models are also an ideal location for any data logic that you may need, such as validation, conversion, etc.
-            </li>
-            <li>
-              <strong>(V) View</strong> - A View is any type of component that is visually represented. For instance, grids, trees and panels are all considered Views.
-            </li>
-            <li>
-              <strong>(C) Controller</strong> - Controllers are used as a place to maintain the view's logic that makes your app work.
-              This could entail rendering views, routing, instantiating Models, and any other sort of app logic.
-            </li>
-
-            <li>
-              <strong>(VM) ViewModel</strong> - The ViewModel is a class that manages data specific to the View.
-              It allows interested components to bind to it and be updated whenever this data changes.
-            </li>
-          </ul>
-        </p>
-
-        <h3>Models and Stores</h3>
-        <p>
-          <code>Models</code> and <code>Stores</code> make up the information gateway of your application.
-          Most of your data is sent, retrieved, organized, and "modeled" by these two classes.
-        </p>
-        <p>
-          A <code>Model</code> represents any type of persist-able data in your application.
-          Each model has fields and functions that allow your application to "model" data.
-          Models are most commonly used in conjunction with stores.
-          Stores can then be consumed by data-bound components like grids, trees, and charts.
-        </p>
-        <p>
-          A <code>Store</code> is a client side cache of records (instances of a Model class).
-          Stores provide functions for querying the records contained within.
-        </p>
-
-        <h3>ViewControllers</h3>
-        <p>Extension React delivers some exciting improvements for React. To enhance MVC applications, Extension React provides ViewControllers, also called as Component:</p>
-        <p>
-          <ul className="list-style-disc">
-            <li>Simplifies the connection to views using “Component” decorator.</li>
-            <li>Leverages the life cycle of views to automatically manage their associated.</li>
-            <li>Reduces complexity based on a one-to-one relationship with the managed view.</li>
-            <li>Provides encapsulation to make nesting views reliable.</li>
-            <li>Retains the ability to select components and listen to their events at any level below the associated view.</li>
-          </ul>
-        </p>
-
-        <pre className="editor">{`// ./components/search/search.js
+    return <Container className="main container scrollable">
+      <h1 className="mb-md">Application Architecture</h1>
+      <p className="mb-md">
+        Extension React provides support for MVC+VM application architectures. 
+        To understand what is MVC+VM, we should start by further defining what the various abbreviations represent.
+      </p>
+      <ul className="mb-md">
+        <li>
+          <strong>(M) Model</strong> - This is the data for your application.
+          A set of classes (called "Models") defines the fields for their data (e.g. a User model with user-name and password fields).
+          Models know how to persist themselves through the data package and can be linked to other models via associations.
+          Models are normally used in conjunction with Stores to provide data for grids and other components.
+          Models are also an ideal location for any data logic that you may need, such as validation, conversion, etc.
+        </li>
+        <li>
+          <strong>(V) View</strong> - A View is any type of component that is visually represented. For instance, grids, trees and panels are all considered Views.
+        </li>
+        <li>
+          <strong>(C) Controller</strong> - Controllers are used as a place to maintain the view's logic that makes your app work.
+          This could entail rendering views, routing, instantiating Models, and any other sort of app logic.
+        </li>
+        <li>
+          <strong>(VM) ViewModel</strong> - The ViewModel is a class that manages data specific to the View.
+          It allows interested components to bind to it and be updated whenever this data changes.
+        </li>
+      </ul>
+      <h3 className="mb-md">Models and Stores</h3>
+      <p className="mb-md">
+        <code>Models</code> and <code>Stores</code> make up the information gateway of your application.
+        Most of your data is sent, retrieved, organized, and "modeled" by these two classes.
+      </p>
+      <p className="mb-md">
+        A <code>Model</code> represents any type of persist-able data in your application.
+        Each model has fields and functions that allow your application to "model" data.
+        Models are most commonly used in conjunction with stores.
+        Stores can then be consumed by data-bound components like grids, trees, and charts.
+      </p>
+      <p className="mb-md">
+        A <code>Store</code> is a client side cache of records (instances of a Model class).
+        Stores provide functions for querying the records contained within.
+      </p>
+      <h3 className="mb-md">ViewControllers</h3>
+      <p className="mb-md">Extension React delivers some exciting improvements for React. To enhance MVC applications, Extension React provides ViewControllers, also called as Component:</p>
+      <ul className="mb-md">
+        <li>Simplifies the connection to views using “Component” decorator.</li>
+        <li>Leverages the life cycle of views to automatically manage their associated.</li>
+        <li>Reduces complexity based on a one-to-one relationship with the managed view.</li>
+        <li>Provides encapsulation to make nesting views reliable.</li>
+        <li>Retains the ability to select components and listen to their events at any level below the associated view.</li>
+      </ul>
+      <pre>
+{`// ./components/search/search.js
 import React, { Component } from 'react';
 import Rext, { Container } from 'ext-react';
 import DataStore from '~/stores/data';
@@ -164,8 +155,8 @@ export default class SearchResult extends Component {
       </Grid>
     </Container>
   }
-}`}</pre>
-      </Container>
+}`}
+      </pre>
     </Container>
   }
 }
